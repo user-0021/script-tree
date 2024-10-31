18fd498e77748a21e40a35cdfc5a87ed3fef1a98
user-0021 <nabeyamaoden@gmail.com>
2024-10-31 17:20:21 +0900

    add Readme

--- src/main.c
+++ src/main.c
@@ -7,27 +7,28 @@
 
 typedef struct{
 	float scale;
-	int32_t width;
-	int32_t height;
-	uint32_t x;
-	uint32_t y;
+	int64_t width;
+	int64_t height;
+	float x;
+	float y;
 }mainWindowData;
 
 FTGLfont * baseFont;
 
 void MainWindow(char opcode,WINDOW_HANDLE handle
 		,uint64_t oprand1,uint64_t oprand2,void* userData){
-	const static int period = 40;
+	const static int period = 120;
 	const static int radix  = 3;
 	const static int pieces = 40;
 	const static int pieces_max = 40;
+	const static float zoomScale = 0.1;
 	const static unsigned int mills = 100;
 
 	mainWindowData* data = userData;
 	switch(opcode){
 		case WINDOW_MESSAGE_CREATE:{
 			data->scale = 1;
-			data->x = 0;
+			data->x = 200;
 			data->y = 0;
 			wtRegistTimer(handle,mills);
 			break;
@@ -46,13 +47,16 @@ void MainWindow(char opcode,WINDOW_HANDLE handle
 			if(!period_scale)
 				period_scale = 1;
 			
-			unsigned int dotRow    = data->width  / period_scale;
-			unsigned int dotColumn = data->height / period_scale;		
-			int x = period_scale;
-			int* y = malloc(sizeof(int) * dotColumn);		
+			unsigned int dotRow    = (data->width  / period_scale) + 1;
+			unsigned int dotColumn = (data->height / period_scale) + 1;		
+			
+			int32_t x = ((int)(data->x * data->scale)) % period_scale;
+			int32_t* y = malloc(sizeof(int32_t) * dotColumn);		
+			y[0] = ((int)(data->y * data->scale)) % period_scale;
+			
+
 			int r = radix * data->scale;
 			int p = pieces * data->scale;
-			y[0] = period_scale;
 
 			if(p > pieces_max)
 				p = pieces_max;
@@ -64,8 +68,9 @@ void MainWindow(char opcode,WINDOW_HANDLE handle
 					for(j = 0;j < dotColumn;j++){
 						if(!i && j)
 							y[j] = y[j-1] + period_scale;
-
-						wtDrawCircle(handle,x,y[j],r,p);
+						
+						if(!(x & 0x80000000 || y[j] & 0x80000000))
+							wtDrawCircle(handle,x,y[j],r,p);
 					}
 
 					x += period_scale;
@@ -86,14 +91,16 @@ void MainWindow(char opcode,WINDOW_HANDLE handle
 			switch(oprand2&0xF){
 				case 3:{
 					if(oprand2&0xF00000000)
-						if(data->scale > 0.11)
-							data->scale -= 0.1;
+						if(data->scale > 0.11){
+							data->scale -= zoomScale;
+						}
 					break;
 				}
 				case 4:{
 					if(oprand2&0xF00000000)
-						if(data->scale < 9.99)
-							data->scale += 0.1;
+						if(data->scale < 9.99){
+							data->scale += zoomScale;
+						}
 					break;
 				}
 				default:
