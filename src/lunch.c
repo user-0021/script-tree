4232f5a121a4e1c17ef5769d065f04b20aadbcd6
user-0021 <nabeyamaoden@gmail.com>
2024-11-18 14:22:59 +0900

    nonblocking input

--- src/lunch.c
+++ src/lunch.c
@@ -2,9 +2,19 @@
 #include <string.h>
 #include <script-tree.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int parsArgment(char* str,int size,char* argv[]);
+typedef struct{
+	char* name;
+	int fd[2];
+}nodeData;
 
+//module
+static int parsArgment(char* str,const int size,char* argv[]);
+static int popenRW(const char const * command,int* fd);
+
+//command callback
 static void help(int* argc,char* argv[]);
 static void quit(int* argc,char* argv[]);
 static void save(int* argc,char* argv[]);
@@ -26,40 +36,61 @@ static uint8_t exit_flag = 0;
 void lunch(int* argc,char* argv[]){
 	printf("lunch success.\n");
 	
+	//init terminal
 	char inputData[1024];
-	memset(inputData,0,sizeof(inputData));
+	size_t inputDataSize = sizeof(inputDataSize);
 	char* args[5];
+	int oldInFlag = fcntl(STDIN_FILENO,F_GETFL);
+	fcntl(STDIN_FILENO,F_SETFL,oldInFlag|O_NONBLOCK);
+	putchar('>');
+
+	//loop
 	while(1){
 		if(exit_flag)
 			break;
-		putchar('>');
 
+		/*--------------nodeSystemBegin-------------*/
+
+		/*---------------nodeSystemEnd--------------*/
+		
+
+		/*---------------terminalBegin--------------*/
 		//get input
-		size_t size = sizeof(inputData);
-		fgets(inputData,size,stdin);
-		inputData[strlen(inputData) - 1] = '\0';
-		int count = parsArgment(inputData,sizeof(args)/sizeof(char*),args);
-		
-		//continue when no input
-		if(!count)
-			continue;
+		if(fgets(inputData,sizeof(inputData),stdin) != NULL){
+			inputData[strlen(inputData) - 1] = '\0';//replase \n to \0
+			int count = parsArgment(inputData,sizeof(args)/sizeof(char*),args);
+			
+			//continue when no input
+			if(!count)
+				continue;
 
-		//check comand input to call command function
-		int i;
-		int commandCount = sizeof(commandList)/sizeof(Command);
-		for(i = 0;i < commandCount;i++){
-			if(strcmp(args[0],commandList[i].argName) == 0){
-				commandList[i].func(&count,args);
-				break;
+			//check comand input to call command function
+			int i;
+			int commandCount = sizeof(commandList)/sizeof(Command);
+			for(i = 0;i < commandCount;i++){
+				if(strcmp(args[0],commandList[i].argName) == 0){
+					commandList[i].func(&count,args);
+					break;
+				}
 			}
-		}
 		
-		if(i == commandCount)
-			printf("%s is invalide token\n",args[0]);
+			if(i == commandCount)
+				printf("%s is invalide token\n",args[0]);
+
+			//terminal head
+			putchar('>');
+		}
+		/*----------------terminalEnd---------------*/
 	}
+
+	//return
+	putchar('\n');
+
+	//reset
+	fcntl(STDIN_FILENO,F_SETFD,oldInFlag);
 }
 
-int parsArgment(char* str,int size,char* argv[]){
+static int parsArgment(char* str,const int size,char* argv[]){
 
 	int i;
 	for(i = 0;i < size;i++){
@@ -108,6 +139,7 @@ static void load(int* argc,char* argv[]){
 }
 
 static void run(int* argc,char* argv[]){
+
 }
 
 static void connect(int* argc,char* argv[]){
